4e6a9af0ed65807344882d4241ece1f6d8602e51
gurjot <gurjotsaini53@gmail.com>
2023-09-12 11:02:08 +0530

    revert with container

---
+++ JAVA/src/_Sorting/RevertWithContainer.java
@@ -0,0 +1,38 @@
+package _Sorting;
+import java.util.Stack;
+public class RevertWithContainer {
+
+
+    public static void RevertWithContainer(int arr[], int size)
+    {
+        Stack<Integer> s1= new Stack<>();
+
+        for (int x:arr)
+        {
+            s1.add(x);
+        }
+
+        for (int i=0;i<size;i++)
+        {
+            arr[i]=s1.pop();
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int size = 5;
+
+        for (int x:arr)
+        {
+            System.out.print(x+ " ");
+        }
+        System.out.println();
+        
+        RevertWithContainer(arr,size);
+
+        for (int x:arr)
+        {
+            System.out.print(x+ " ");
+        }
+        System.out.println();
+    }
+}
