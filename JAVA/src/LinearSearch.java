3d288d307f531c9844bd0efa6c7465391640a984
gurjot <gurjotsaini53@gmail.com>
2023-09-12 11:46:57 +0530

    learning searching linear and binary search

---
+++ JAVA/src/LinearSearch.java
@@ -0,0 +1,27 @@
+public class LinearSearch {
+
+
+    public static int getSearchResult(int arr[], int size , int key){
+        for (int i=0;i<size;i++)
+        {
+            if(arr[i]==key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int size =5;
+        int key=4;
+        int res = getSearchResult(arr,size,key);
+
+        if (res==-1)
+        {
+            System.out.println("Element not found");
+        }
+        else
+            System.out.println("Element found at "+ res + " index");
+    }
+}
---
+++ JAVA/src/_Strings/BinarySearch.java
@@ -0,0 +1,40 @@
+package _Strings;
+
+public class BinarySearch {
+
+    public static int getSearchResult(int arr[], int size , int key){
+
+        int s=0;
+        int e=size-1,mid;
+
+        while (s<=e)
+        {
+            //mid=(s+e)/2;
+            mid=s+(e-s)/2;
+            if (arr[mid]==key)
+            {
+                return mid;
+            }
+            else if (arr[mid]>key)
+                e=mid-1;
+            else
+                s=mid+1;
+        }
+
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};  // arr must be sorted in binary search
+        int size =5;
+        int key=6;
+        int res = getSearchResult(arr,size,key);
+
+        if (res==-1)
+        {
+            System.out.println("Element not found");
+        }
+        else
+            System.out.println("Element found at "+ res + " index");
+    }
+}
