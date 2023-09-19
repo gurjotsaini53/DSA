2e6467de5a9c77e1f0fc8dacccf9ed2477e1f77a
gurjot <gurjotsaini53@gmail.com>
2023-09-19 12:35:18 +0530

    Adding prime number logic with diffreenr approaches

---
+++ JAVA/src/_Math/PrimeNumbers.java
@@ -0,0 +1,29 @@
+package _Math;
+
+public class PrimeNumbers {
+    public static void checkPrime(int n)
+    {
+        boolean flag = true;
+//        for (int i=0;i<=n-1;i++)
+//        for (int i=0;i<=Math.pow(n,0.5);i++)
+//        for (int i=0;i*i<=n;i++)
+//        for (int i=0;i<=n/i;i++)
+
+
+        {
+
+            if (n%i==0) {
+                flag = false;
+                break;
+            }
+
+        }
+
+        if (flag) System.out.println("Prime Number");
+        else System.out.println("Not Prime Number");
+    }
+    public static void main(String [] args)
+    {
+
+    }
+}
