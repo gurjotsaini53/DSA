6d4f720278d56271354c2a6dcfac202d94557394
gurjot <gurjotsaini53@gmail.com>
2023-10-03 22:32:58 +0530

    code improvement

--- JAVA/src/BinarySearchProblems/CountOccurenceBinarySearch.java
+++ JAVA/src/BinarySearchProblems/CountOccurenceBinarySearch.java
@@ -9,15 +9,15 @@ public class CountOccurenceBinarySearch {
         int last = LastOccurence.Last(arr,k);
         if (first==-1 || last==-1)
         {
-            return -1;
+            return 0;
 
         }
 
         return last-first+1;
     }
     public static void main(String[] args) {
-        int arr[] =  new int[]{1,1,2,2,3,3,8,8,10,10,10,10};
-        int k =10;
+        int arr[] =  new int[]{1,1,2,2,3,3,8,8,10,10,10};
+        int k =3;
         System.out.println( k + " is found " + Count(arr,k)  + " times");
     }
 }
