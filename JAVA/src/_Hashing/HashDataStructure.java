fa094f67d5b03301417b7b6844ce8dbe9e95b585
gurjot <gurjotsaini53@gmail.com>
2023-10-10 10:00:08 +0530

    learning separated chaining in hashing

--- JAVA/src/_Hashing/HashDataStructure.java
+++ JAVA/src/_Hashing/HashDataStructure.java
@@ -27,8 +27,74 @@ public class HashDataStructure {
     private int occupiedBuckets;
 
 
+   public HashDataStructure(int capacity)
+   {
+       this.size=0;
+       this.totalBucketSize = capacity;
+       this.buckets = new HashNode[totalBucketSize];
+   }
 
 
+
+   public int size()
+   {
+       return size;
+   }
+
+   public boolean isEmpty(){
+       return size==0;
+   }
+
+   public int hashCode(Integer key)
+   {
+       return key%totalBucketSize;
+   }
+
+   public int getBucketIndex(Integer key)
+   {
+       return hashCode(key);
+   }
+
+   private int getTotalBucketSize(){
+       return totalBucketSize;
+   }
+
+   private int getOccupiedBuckets()
+   {
+       return occupiedBuckets;
+   }
+
+   private int getEmptyBuckets(){
+       return emptyBuckets;
+   }
+
+
+
+   // 4 main methods of hashmap data strucures.
+
+
+ public void put(Integer key,String value){
+
+ }
+    public void get(Integer key){
+
+    }
+    public void  set(Integer key){
+
+    }
+    public void search(Integer key)
+    {
+
+    }
+
+    public void delete(Integer key)
+    {
+        
+    }
+    public void show()
+    {
+
+    }
 
 
     public static void main(String[] args) {
