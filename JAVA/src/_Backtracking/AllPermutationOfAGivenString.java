19c27cd8248a96886d3eb7c2e8c977f188ac2a46
gurjot <gurjotsaini53@gmail.com>
2023-10-16 00:07:38 +0530

    string permutation

--- JAVA/src/_Backtracking/AllPermutationOfAGivenString.java
+++ JAVA/src/_Backtracking/AllPermutationOfAGivenString.java
@@ -1,4 +1,24 @@
 package _Backtracking;
 
 public class AllPermutationOfAGivenString {
+
+
+    public static void FindPermutation(String str, String perm , int index)
+    {
+        if (str.length()==0)
+        {
+            System.out.println(perm);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++)
+        {
+            char current = str.charAt(i);
+             String newStr = str.substring(0,i) + str.substring(i+1);
+            FindPermutation(newStr,perm+current,index+1);
+        }
+    }
+    public static void main(String[] args) {
+        FindPermutation("ABC","",0);
+    }
 }
