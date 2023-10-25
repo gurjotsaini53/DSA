bdf06f7b250cd17d3d61aadfce5bbc891ce50f74
gurjot <gurjotsaini53@gmail.com>
2023-10-25 09:46:39 +0530

    demo

---
+++ JAVA/src/_Array/_ArrayQues/Sorting/Demo.java
@@ -0,0 +1,15 @@
+package _Array._ArrayQues.Sorting;
+
+public class Demo {
+
+
+    private  int age;
+    public Demo(){
+        age=10;
+    }
+
+    public static void main(String[] args) {
+          Demo obj = new Demo();
+        System.out.println(obj.age);
+    }
+}
